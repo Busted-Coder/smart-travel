e28405f7bf530b200db251bf7f18f245e1332e3f
aa7985 <30986722+aa7985@users.noreply.github.com>
2017-08-18 00:00:30 +0500

    V 1.0

--- application/controllers/auth.php
+++ application/controllers/auth.php
@@ -6,6 +6,9 @@ class Auth extends MY_Controller {
 	public function __construct()
 	{
 		parent::__construct();
+		$this->OnlyAdmin();
+		$this->load->model('auth_model','AuthModel');
+
 	}
 	public function login_loader(){
 		$this->loadView('login/login_register',null,false);
@@ -13,12 +16,17 @@ class Auth extends MY_Controller {
 	public function login(){
 		$username = $this->input->post('username');
 		$password = $this->input->post('password');
+		echo $username;
+		echo $password;
+		$db_users = $this->AuthModel->login();
+		echo count($db_users);
 		if($username || $password){
-			if($username == 'admin' && $password == 'admin'){
-				$_SESSION['Role'] = 'Admin';
-				redirect('index.php/role/lists');
+			if($username == $db_users['email'] && $password == $db_users['password']){
+				$_SESSION['Role'] = 'User';
+				$_SESSION['User'] = $db_users['email'];
+				redirect('index.php/Welcome/index');
 			}else{
-				$this->load->view('admin/login', array('Error'=> 'Incorrect Username or password!'));
+				$this->loadView('login/login_register', array('Error'=> 'Incorrect Username or password!'),false);
 			}
 		}else{
 			$this->load->view('admin/login', array('Error'=>''));
